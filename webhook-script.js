58a435717f4e6b609edea6ab7180b2c67a77ba9e
onurookie35 <onurcaylakofficial@gmail.com>
2025-09-28 01:56:58 +0300

    Update webhook-script.js

--- webhook-script.js
+++ webhook-script.js
@@ -225,7 +225,7 @@ const WEEKLY_MESSAGES = {
             image_url: "https://tr-wiki.metin2.gameforge.com/images/3/3d/K%C3%B6t%C3%BC_Ruh_Kovma_Ka%C4%9F%C4%B1d%C4%B1.png"
         },
         {
-            hour: 01, minute: 56,
+            hour: 01, minute: 58,
             code: "Bu mesaj bot tarafından gönderilmiştir.",
             title: "13:00 - 17:00 (4 Saat Sürecek)",
             message: "Arttırma Kağıdı eventi başladı.",
@@ -233,7 +233,7 @@ const WEEKLY_MESSAGES = {
             image_url: "https://tr-wiki.metin2.gameforge.com/images/7/78/Artt%C4%B1rma_Ka%C4%9F%C4%B1d%C4%B1.png"
         },
         {
-            hour: 10, minute: 00,
+            hour: 01, minute: 59,
             code: "Bu mesaj bot tarafından gönderilmiştir.",
             title: "17:00 - 21:00 (4 Saat Sürecek)",
             message: "Kötü Ruh Kovma Kağıdı eventi başladı.",
