c398c629a89f66e0a6a7b067a53474afbb6fc324
onurookie35 <onurcaylakofficial@gmail.com>
2025-09-28 09:06:59 +0300

    Update webhook-script.js

--- webhook-script.js
+++ webhook-script.js
@@ -217,7 +217,7 @@ const WEEKLY_MESSAGES = {
     ],
     6: [ // Pazar
         {
-            hour: 8, minute: 49,
+            hour: 9, minute: 12,
             code: "Bu mesaj bot tarafından gönderilmiştir.",
             title: "01:00 - 13:00 (12 Saat Sürecek)",
             message: "Kötü Ruh Kovma Kağıdı eventi başladı.",
@@ -225,7 +225,7 @@ const WEEKLY_MESSAGES = {
             image_url: "https://tr-wiki.metin2.gameforge.com/images/3/3d/K%C3%B6t%C3%BC_Ruh_Kovma_Ka%C4%9F%C4%B1d%C4%B1.png"
         },
         {
-            hour: 8, minute: 50,
+            hour: 9, minute: 14,
             code: "Bu mesaj bot tarafından gönderilmiştir.",
             title: "13:00 - 17:00 (4 Saat Sürecek)",
             message: "Arttırma Kağıdı eventi başladı.",
@@ -233,7 +233,7 @@ const WEEKLY_MESSAGES = {
             image_url: "https://tr-wiki.metin2.gameforge.com/images/7/78/Artt%C4%B1rma_Ka%C4%9F%C4%B1d%C4%B1.png"
         },
         {
-            hour: 8, minute: 59,
+            hour: 9, minute: 16,
             code: "Bu mesaj bot tarafından gönderilmiştir.",
             title: "17:00 - 21:00 (4 Saat Sürecek)",
             message: "Kötü Ruh Kovma Kağıdı eventi başladı.",
@@ -241,7 +241,7 @@ const WEEKLY_MESSAGES = {
             image_url: "https://tr-wiki.metin2.gameforge.com/images/3/3d/K%C3%B6t%C3%BC_Ruh_Kovma_Ka%C4%9F%C4%B1d%C4%B1.png"
         },
         {
-            hour: 8, minute: 56,
+            hour: 9, minute: 18,
             code: "Bu mesaj bot tarafından gönderilmiştir.",
             title: "21:00 - 01:00 (4 Saat Sürecek)",
             message: "Liderin Kitabı eventi başladı.",
