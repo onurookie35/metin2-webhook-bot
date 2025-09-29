e8dae5a696723e231624c3b7477bdc2c87e8ed4e
onurookie35 <onurcaylakofficial@gmail.com>
2025-09-29 11:13:14 +0300

    Update webhook-script.js

--- webhook-script.js
+++ webhook-script.js
@@ -14,7 +14,7 @@ if (!WEBHOOK_URL) {
 const WEEKLY_MESSAGES = {
     0: [ // Pazartesi
         {
-            hour: 11, minute: 10,
+            hour: 01, minute: 00,
             code: "Bu mesaj bot tarafından gönderilmiştir.",
             title: "01:00 - 13:00 (12 Saat Sürecek)",
             message: "Münzevi Tavsiyesi eventi başladı.",
